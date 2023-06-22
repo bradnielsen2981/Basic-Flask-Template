e0d71462c5cd01e18f6a317dc77d351046c78092
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-06-22 11:10:40 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import *
+import sys
+import logging
+
+
+#---CONFIGURE APP---------------------------------------------------
+app = Flask(__name__)
+logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
+sys.tracebacklimit = 10
+
+#---VIEW FUNCTIONS----------------------------------------------------
+@app.route('/')
+def home():
+    return "HELLO"
+
+
+#main method called web server application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) #runs a local server on port 5000
