edcb20d0d32a33e79847ac34b4ee53a58e7f6a79
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-06-25 21:32:44 +1000

    commit

--- app.py
+++ app.py
@@ -11,11 +11,22 @@ sys.tracebacklimit = 10
 @app.route('/')
 def login():
     app.logger.info("Login")
-    return render_template("login.html", message="Please login")
+    return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     app.logger.info("Register")
+
+    if request.method == "POST":
+
+        fname = request.form['fname']
+        lname = request.form['lname']
+        password = request.form['password']
+        passwordconfirm = request.form['passwordconfirm']
+        email = request.form['email']
+
+        app.logger.info(email)
+
     return render_template("register.html", message="Please register")
 
 #main method called web server application
