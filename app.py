09d1027df2f1f4ed09ea5547b244fac02df039fc
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-06-25 18:47:19 +1000

    commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import *
 import sys
 import logging
 
-
 #---CONFIGURE APP---------------------------------------------------
 app = Flask(__name__)
 logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
@@ -10,10 +9,10 @@ sys.tracebacklimit = 10
 
 #---VIEW FUNCTIONS----------------------------------------------------
 @app.route('/')
-def home():
-    return "HELLO"
-
+def login():
+    app.logger.info("LOGIN")
+    return "Hello World"
 
 #main method called web server application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) #runs a local server on port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True) #runs a local server on port 5000
