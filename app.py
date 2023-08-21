e62dee713634a41f75c59d9c85c2c0acd745e43e
Brad Nielsen <brad_nielsen29@hotmail.com>
2023-08-21 14:59:35 +1000

    c

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
 sys.tracebacklimit = 10
 
 # Configure the upload folder and allowed file extensions
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'profilephotos'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -75,8 +75,6 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
-
-
         results = DATABASE.ViewQuery("SELECT * FROM users WHERE email = ?", (email,))
         if results:
             userdetails = results[0] #row in the user table (Python Dictionary)
@@ -86,6 +84,7 @@ def login():
                 session['permission'] = userdetails['permission']
                 session['userid'] = userdetails['userid']
                 session['name'] = userdetails['firstname'] + " " + userdetails['lastname']
+                session['profilephoto'] = userdetails['profilephoto']
 
                 if session['permission'] == 'admin':
                     return redirect('./admin')
@@ -117,51 +116,37 @@ def register():
             if results:
                 message = "Error, user already exists"
             else:
+
+                #UPLOAD A FILE
+                filepath = ''
+                app.logger.info(request.files)
+                if 'file' in request.files:
+                    
+                    file = request.files['file']
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                        file.save(filepath)
+                        flash("File uploaded successfully")
+                    else:
+                        flash("Problem with file upload")
+                else:
+                    flash("File not found")
+
                 password = hash_password(password)
-                DATABASE.ModifyQuery("INSERT INTO users (firstname, lastname, email, password) VALUES (?,?,?,?)", (firstname, lastname, email, password))
+                DATABASE.ModifyQuery("INSERT INTO users (firstname, lastname, email, password, profilephoto) VALUES (?,?,?,?,?)", (firstname, lastname, email, password,filepath))
                 message = "Success, users has been added"
                 return redirect('./')
 
     return render_template("register.html", message=message)
 
-
-#FILE UPLOADING
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # Check if a file is present in the request
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        
-        file = request.files['file']
-        
-        # Check if the file is empty
-        if file.filename == '':
-            flash('No file selected')
-            return redirect(request.url)
-        
-        # Check if the file is allowed and save it
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            flash('File successfully uploaded')
-            return redirect(url_for('upload_file'))
-        else:
-            flash('Allowed file types are png, jpg, jpeg, gif')
-    
-    return 
-
-    ''' TO BE PLACED IN HTML
-    <!doctype html>
-    <title>Upload File</title>
-    <h1>Upload File</h1>
-    <form method="post" action="/upload" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <input type="submit" value="Upload">
-    </form>
-    '''
+#return a profile photo
+@app.route('/profilephotos/<filename>')
+def serve_file(filename):
+    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)): # Ensure the file exists
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    else:
+        abort(404) # If the file does not exist, return a 404 error
 
 #main method called web server application
 if __name__ == '__main__':
