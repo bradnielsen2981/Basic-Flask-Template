cbdd8b83ea3c054f3fb64b4110b6da14a8a174b5
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-05-08 10:09:48 +1000

    commit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import *
-import sys, os
-import logging
+import sys, os, logging, datetime
 from interfaces.databaseinterface import Database
 from interfaces.hashing import *
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 #---CONFIGURE APP---------------------------------------------------
 app = Flask(__name__)
+cors = CORS(app)
 logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
 sys.tracebacklimit = 10
 
@@ -21,7 +22,9 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-DATABASE = Database("database/test.db", app.logger)
+DATABASE = Database("database/karaoke.db", app.logger)
+
+#use @cross_origin() after @app.route to allow external access 
 
 #---VIEW FUNCTIONS----------------------------------------------------
 @app.route('/logout')
@@ -30,6 +33,7 @@ def logout():
     session.clear()
     return redirect('./')
 
+#admin page
 @app.route('/admin', methods=["GET","POST"])
 def admin():
 
@@ -51,6 +55,7 @@ def admin():
     app.logger.info("Admin")
     return render_template("admin.html", results=results)
 
+#home page
 @app.route('/home')
 def home():
 
@@ -60,6 +65,7 @@ def home():
     app.logger.info("Home")
     return render_template("home.html")
 
+#login page
 @app.route('/', methods=["GET","POST"])
 def login():
     app.logger.info("Login")
@@ -96,6 +102,7 @@ def login():
 
     return render_template("login.html", message=message)
 
+#register
 @app.route('/register', methods=['GET','POST'])
 def register():
     app.logger.info("Register")
@@ -139,6 +146,50 @@ def register():
 
     return render_template("register.html", message=message)
 
+#events page
+@app.route('/events', methods=['GET','POST'])
+def events():
+    app.logger.info("Events")
+    if request.method == "POST":
+        eventname = request.form['eventname']
+        eventdatetime = request.form['eventdatetime']
+        eventlocationname = request.form['eventlocationname']
+        eventaddress = request.form['eventaddress']
+        eventpostcode = request.form['eventpostcode']
+        eventlongitude = request.form['eventlongitude']
+        eventlatitude = request.form['eventlatitude']
+        eventcreator = session['userid']
+        DATABASE.ModifyQuery("INSERT INTO events (eventname, eventdatetime, eventlocationname, eventaddress, eventpostcode, eventlongitude, eventlatitude, eventcreator) VALUES (?,?,?,?,?,?,?,?)", (eventname, 
+        eventdatetime, eventlocationname, eventaddress, eventpostcode, eventlongitude, eventlatitude, eventcreator))
+
+        #allow deletion of events by the creator
+        selectedevents = request.form.getlist("selectedevents")
+        for eventid in selectedevents:
+            DATABASE.ModifyQuery("DELETE FROM events WHERE eventid = ? AND eventcreator = ?", (eventid,session['userid']))
+
+    results = DATABASE.ViewQuery("SELECT * FROM events WHERE eventdatetime > datetime('now') ORDER BY eventdatetime ASC")
+    return render_template("events.html", results=results)
+
+#playlist page
+@app.route('/playlist/<eventid>', methods=['GET','POST'])
+def playlist(eventid):
+    app.logger.info("Playlist")
+    if request.method == "POST":
+        songname = request.form['songname']
+        artistname = request.form['artistname']
+        performername = request.form['performername']
+        requesteddatetime = datetime.datetime.now()
+        DATABASE.ModifyQuery("INSERT INTO playlist (songname, artistname, performername, requesteddatetime, eventid) VALUES (?,?,?,?,?)", (songname, artistname, performername, requesteddatetime, eventid)
+        return redirect('./playlist/'+eventid)
+
+        #allow deletion of events by the creator
+        selectedsongids = request.form.getlist("selectedsongs")
+        for songid in selectedsongids:
+            DATABASE.ModifyQuery("DELETE FROM playlist WHERE songid = ? AND eventcreator = ?", (eventid,session['userid']))
+
+    results = DATABASE.ViewQuery("SELECT * FROM playlist WHERE eventid = ? AND current=1", (eventid,))
+    return render_template("playlist.html", results=results, eventid=eventid)
+
 #return a profile photo
 @app.route('/profilephotos/<filename>')
 def serve_file(filename):
@@ -147,6 +198,14 @@ def serve_file(filename):
     else:
         abort(404) # If the file does not exist, return a 404 error
 
+# Exit the web server
+@app.route('/exit', methods=['GET','POST'])
+def exit():
+    app.logger.info("Exiting")
+    func = request.environ.get('werkzeug.server.shutdown')
+    func()
+    return jsonify({'message':'Exiting'})
+
 #main method called web server application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) #runs a local server on port 5000
+    app.run(host='0.0.0.0', port=3000, debug=True) #runs a local server
